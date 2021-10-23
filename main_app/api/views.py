bcdc6313671bf339d93160d0271dbe7836282d3e
4mal1ne <parieiko.vlad@gmail.com>
2021-10-24 01:36:25 +0300

    Delete 'viewset' method. Wrote view classes on 'generics' method. Added some description.

--- main_app/api/views.py
+++ main_app/api/views.py
@@ -1,25 +1,21 @@
-from rest_framework import viewsets
+from rest_framework import generics
 
 from .serializers import (
     WorkSerializer,
     CommentsSerializer
 )
-from ..models import Works, Comments
+# from ..models import Works, Comments
 
 
-class WorkViewSet(viewsets.ModelViewSet):
+class WorkCreateView(generics.CreateAPIView):
     """
     Collecting the data stack and displaying it on the page.
     """
-    queryset = Works.objects.all()
     serializer_class = WorkSerializer
 
 
-class CommentsViewSet(viewsets.ModelViewSet):
+class CommentsCreateView(generics.CreateAPIView):
     """
     Collecting the data stack and displaying it on the page.
     """
-    queryset = Comments.objects.all()
     serializer_class = CommentsSerializer
-
-
