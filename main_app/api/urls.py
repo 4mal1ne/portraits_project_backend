dbcf9c102cb13c37f306c964570b51bd7b9d5aa8
4mal1ne <parieiko.vlad@gmail.com>
2021-10-24 01:35:10 +0300

    Commit 'routing' method. Added app name and default url path for Works and Comments view classes.

--- main_app/api/urls.py
+++ main_app/api/urls.py
@@ -1,9 +1,18 @@
-from rest_framework import routers
-from .views import WorkViewSet, CommentsViewSet
+# from rest_framework import routers
+# from .views import WorkViewSet, CommentsViewSet
+#
+# router = routers.SimpleRouter()
+# router.register('artworks', WorkViewSet, basename='artworks')  # register "Work viewSet" in router in url path
+# router.register('comments', CommentsViewSet, basename='comments')  # register "Comments viewSet" in router in url path
+#
+# urlpatterns = []
+# urlpatterns += router.urls  # Add registered "urls" to the main routing.
 
-router = routers.SimpleRouter()
-router.register('artworks', WorkViewSet, basename='artworks')  # register "Work viewSet" in router in url path
-router.register('comments', CommentsViewSet, basename='comments')  # register "Comments viewSet" in router in url path
+from django.urls import path, include
+from .views import *
 
-urlpatterns = []
-urlpatterns += router.urls  # Add registered "urls" to the main routing.
+app_name = 'artworks'
+urlpatterns = [
+    path('artworks/', WorkCreateView.as_view()),  # route Work view class.
+    path('comments/', CommentsCreateView.as_view()),
+]
