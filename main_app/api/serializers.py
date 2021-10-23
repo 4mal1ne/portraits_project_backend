4d20e2dd914918dc06a0099b273b865b797d1080
4mal1ne <parieiko.vlad@gmail.com>
2021-10-24 02:47:42 +0300

    Wrote two new serializers for the render all Json data set.

--- main_app/api/serializers.py
+++ main_app/api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from ..models import Works, Comments
 
 
-class WorkSerializer(serializers.ModelSerializer):
+class WorkListSerializer(serializers.ModelSerializer):
     """
     Selection of the desired model and fields to be serialized in Json format.
     """
@@ -13,7 +13,7 @@ class WorkSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class CommentsSerializer(serializers.ModelSerializer):
+class CommentsListSerializer(serializers.ModelSerializer):
     """
     Selection of the desired model and fields to be serialized in Json format.
     """
@@ -21,3 +21,23 @@ class CommentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comments
         fields = '__all__'
+
+
+class WorkSerializer(serializers.ModelSerializer):
+    """
+    Selection of the desired model and fields to be serialized in Json format.
+    """
+
+    class Meta:
+        model = Works
+        fields = '__all__'  # All fields into the model Works
+
+
+class CommentsSerializer(serializers.ModelSerializer):
+    """
+    Selection of the desired model and fields to be serialized in Json format.
+    """
+
+    class Meta:
+        model = Comments  # The heir is model Comments.
+        fields = '__all__'  # All fields into the model Comments
