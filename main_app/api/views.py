adce3a1f2bb1072a970c5d341889ea1c22c55be7
4mal1ne <parieiko.vlad@gmail.com>
2021-10-24 02:49:29 +0300

    Wrote a two new classListView for the render all Json API data set.

--- main_app/api/views.py
+++ main_app/api/views.py
@@ -2,20 +2,32 @@ from rest_framework import generics
 
 from .serializers import (
     WorkSerializer,
-    CommentsSerializer
+    CommentsSerializer,
+    WorkListSerializer,
+    CommentsListSerializer
 )
-# from ..models import Works, Comments
+from ..models import Works, Comments
 
 
 class WorkCreateView(generics.CreateAPIView):
     """
     Collecting the data stack and displaying it on the page.
     """
-    serializer_class = WorkSerializer
+    serializer_class = WorkSerializer  # render all data from this serializer.
 
 
 class CommentsCreateView(generics.CreateAPIView):
     """
     Collecting the data stack and displaying it on the page.
     """
-    serializer_class = CommentsSerializer
+    serializer_class = CommentsSerializer  # render all data from this serializer.
+
+
+class WorksListView(generics.ListAPIView):
+    serializer_class = WorkListSerializer  # The heir for this model is the model WorkListSerializer.
+    queryset = Works.objects.all()  # Take the all data for model WorkListSerializer and save it.
+
+
+class CommentsListView(generics.ListAPIView):
+    serializer_class = CommentsListSerializer  # The heir for this model is the model CommentsListSerializer.
+    queryset = Comments.objects.all()  # Take the all data for model CommentsListSerializer and save it.
