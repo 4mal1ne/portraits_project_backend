a0802a9d4349021c70ce373569efbe0c208934b8
4mal1ne <parieiko.vlad@gmail.com>
2021-11-06 01:50:15 +0300

    Wrote test views.

--- main_app_ui/views.py
+++ main_app_ui/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from main_app.models import *
+
+
+def home_page(request):
+    data = Works.objects.all()
+    return render(request, 'ui_templates/home_page.html', {'data': data})
+
+
+def about_artist(request):
+    pass
+
+
+def portfolio(request):
+    pass
+
+
+def price(request):
+    pass
+
+
+def testimonials(request):
+    pass
