3cb794a9c55c89350a3ae6fb5997894ec84396e4
4mal1ne <parieiko.vlad@gmail.com>
2021-11-08 16:34:29 +0300

    Added static files path.

--- portraits_project_backend/settings.py
+++ portraits_project_backend/settings.py
@@ -149,7 +149,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = []
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'main_app_ui/static/'),
+]
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
